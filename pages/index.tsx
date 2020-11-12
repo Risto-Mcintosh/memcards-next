e9276e1383ebc8b270624ad9c8cb43c106d2708d
Risto Mcintosh <risto.mcintosh@gmail.com>
2020-11-11 20:53:39 -0600

    start flashcard review page

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,9 +8,19 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <section>
-        <h1 className="text-3xl text-green-600">* Deck Name *</h1>
-      </section>
+      <article className="flex flex-col h-full">
+        <h1 className="my-5 text-3xl text-center text-green-600">
+          * Deck Name *
+        </h1>
+        <section className="flex items-start justify-center flex-1 ">
+          <div
+            className="flex items-center justify-center border-2 border-gray-100 shadow-lg rounded-xl"
+            style={{ minHeight: '400px', minWidth: '300px' }}
+          >
+            <p className="mb-2 text-4xl">Card Front</p>
+          </div>
+        </section>
+      </article>
     </Layout>
   );
 }
