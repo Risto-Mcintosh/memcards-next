3e334c06932dafc785f0fea13b899740c0c20d76
Risto Mcintosh <risto.mcintosh@gmail.com>
2020-11-22 21:47:23 -0600

    add edit and create flashcard pages

--- pages/create-flashcard.tsx
+++ pages/create-flashcard.tsx
@@ -9,47 +9,49 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <article className="flex flex-col items-center h-full ">
-        <h1 className="my-10 text-3xl">Edit Flashcard</h1>
+        <h1 className="my-10 text-3xl">Create A New Flashcard</h1>
         <div className="w-full max-w-xs">
           <form>
-            <label className="block mb-2" htmlFor="deck-name">
-              Deck Name:
-            </label>
-            <input
-              className="w-full px-0 mb-6 border-0 border-b"
-              type="text"
-              name="front"
-              id="deck-name"
-              value="Deck 1"
-              readOnly
-            />
+            <select className="w-full mb-6 rounded" name="deck-name" id="">
+              <option value="deck1">deck1</option>
+              <option value="deck2">deck2</option>
+              <option value="deck3">deck3</option>
+            </select>
 
-            <label className="block mb-2" htmlFor="card-front">
-              Front:
+            <label className="sr-only" htmlFor="card-front">
+              Front
             </label>
             <input
               className="w-full mb-6 rounded"
               type="text"
               name="front"
               id="card-front"
+              placeholder="Front"
             />
 
-            <input className="mb-6" type="file" name="image" id="" />
+            <input
+              className="mb-6"
+              type="file"
+              name="image"
+              id=""
+              placeholder="Image"
+            />
 
-            <label className="block mb-2" htmlFor="card-back">
-              Back:
+            <label className="sr-only" htmlFor="card-back">
+              Back
             </label>
             <input
               className="w-full mb-6 rounded"
               type="text"
               name="back"
               id="card-back"
+              placeholder="Back"
             />
             <button
               className="block px-16 py-3 mx-auto text-2xl text-white bg-gray-600 rounded-xl"
               type="submit"
             >
-              Edit
+              Create
             </button>
           </form>
         </div>
