4c9c31efd454f9e16fe4e636a5ad2276a4d3f30c
Risto Mcintosh <risto.mcintosh@gmail.com>
2020-11-28 01:28:58 -0600

    add headlessUi and update menu items

--- components/CreateButton.tsx
+++ components/CreateButton.tsx
@@ -1,4 +1,5 @@
 import { AnimatePresence, motion } from 'framer-motion';
+import { Menu } from '@headlessui/react';
 import * as React from 'react';
 
 export default function CreateButton() {
@@ -10,44 +11,67 @@ export default function CreateButton() {
   return (
     <div className="absolute bottom-0 right-0 mb-8 mr-8">
       <div className="relative flex flex-col items-center">
-        <button
-          className="z-10 w-12 mt-3 focus:outline-none focus-visible:outline-black"
-          aria-label="Create a new deck or new flashcard"
-          onClick={() => setIsOpen((state) => !state)}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            className="origin-bottom-right stroke-1"
-          >
-            <motion.path
-              animate={isOpen ? 'open' : 'closed'}
-              variants={variants}
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
-        </button>
-        <AnimatePresence>
-          {isOpen && (
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1, top: '-100%' }}
-              exit={{ opacity: 0, top: '-50%' }}
-              className="absolute p-2 text-right bg-gray-300 w-36 right-1/4"
-            >
-              <a href="#" className="block">
-                Create Deck
-              </a>
-              <a href="#" className="block">
-                Create Flashcard
-              </a>
-            </motion.div>
+        <Menu>
+          {({ open }) => (
+            <>
+              <Menu.Button
+                className="z-10 w-12 mt-3 focus:outline-none focus-visible:outline-black"
+                aria-label="Create a new deck or new flashcard"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  className="origin-bottom-right stroke-1"
+                >
+                  <motion.path
+                    animate={open ? 'open' : 'closed'}
+                    variants={variants}
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </Menu.Button>
+
+              {open && (
+                <Menu.Items
+                  static
+                  as={motion.div}
+                  initial={{ opacity: 0, y: 50 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  className="absolute text-right border shadow right-1/4 focus:outline-none -top-full"
+                >
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        href="#"
+                        className={`${
+                          active ? 'bg-gray-300' : ''
+                        } block whitespace-nowrap px-2 py-1`}
+                      >
+                        Create Deck
+                      </a>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        href="#"
+                        className={`${
+                          active ? 'bg-gray-300' : ''
+                        } block whitespace-nowrap px-2 py-1`}
+                      >
+                        Create Flashcard
+                      </a>
+                    )}
+                  </Menu.Item>
+                </Menu.Items>
+              )}
+            </>
           )}
-        </AnimatePresence>
+        </Menu>
       </div>
     </div>
   );
--- components/DeckMenu.tsx
+++ components/DeckMenu.tsx
@@ -1,13 +1,59 @@
+import { Menu } from '@headlessui/react';
+import { motion } from 'framer-motion';
 export default function DeckMenu() {
   return (
-    <button aria-label="Deck menu" className="w-5">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-      >
-        <path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
-      </svg>
-    </button>
+    <div className="relative">
+      <Menu>
+        {({ open }) => (
+          <>
+            <Menu.Button aria-label="Deck menu" className="w-5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
+              </svg>
+            </Menu.Button>
+
+            {open && (
+              <Menu.Items
+                as={motion.div}
+                initial={{ height: 0, opacity: 0 }}
+                animate={{ height: 'auto', opacity: 1 }}
+                transition={{ bounce: 0, duration: 0.15 }}
+                static
+                className="absolute top-0 right-0 z-10 bg-white border shadow focus:outline-none"
+              >
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="#"
+                      className={`${
+                        active ? 'bg-gray-300 ' : ''
+                      } block p-2 text-lg whitespace-nowrap`}
+                    >
+                      Rename
+                    </a>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="#"
+                      className={`${
+                        active ? 'bg-gray-300 ' : ''
+                      } block p-2 text-lg whitespace-nowrap`}
+                    >
+                      Delete
+                    </a>
+                  )}
+                </Menu.Item>
+              </Menu.Items>
+            )}
+          </>
+        )}
+      </Menu>
+    </div>
   );
 }
